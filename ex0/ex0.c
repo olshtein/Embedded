be867805c35a5a2fd3fbc2caa4a4e8913a12e48d
avishai1984 <avishai1984@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-04-24 19:32:22 +0000

    updating make file, adding final tar

--- ex0/ex0.c
+++ ex0/ex0.c
@@ -28,11 +28,8 @@ void modify(int list[], int size)
 
 int data[] = {4, 8, 2, 6, 7, 8, 10, 5};
 
-int bla;
-
 void main (void)
 {
-	bla = 0x12345678;
 	int nItems = sizeof(data) / sizeof(int);
 
 	modify(data, nItems);
