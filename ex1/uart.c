789249ef3e380f8ab6077c07a73dd71212533966
avishai1984 <avishai1984@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-05-01 18:46:18 +0000

    initial import of uart driver's files

---
+++ ex1/uart.c
@@ -0,0 +1,17 @@
+#include "uart.h"
+
+#define UART_BASE_ADDR 0x100000
+
+#define THR_OFFSET 0
+#define RBR_OFFSET 0
+#define DLL_OFFSET 0
+#define IER_OFFSET 1
+#define DLH_OFFSET 1
+#define IIR_OFFSET 2
+//#define FCR_OFFSET 2
+#define LCR_OFFSET 3
+//#define MCR_OFFSET 4
+#define LSR_OFFSET 5
+//#define MSR_OFFSET 6
+#define SR_OFFSET 7
+
