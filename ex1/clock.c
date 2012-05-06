694893e0d9e46e169b43cea0170c2912b39158a0
matanpe <matanpe@gmail.com>
2012-05-06 19:06:22 +0000

    Document the code

--- ex1/clock.c
+++ ex1/clock.c
@@ -3,16 +3,25 @@
 
 #define CLOCK_ADDR 0x100
 
+/*
+ * Initilize the clock to the time "0"
+ */
 void clockInit()
 {
 	clockSetTime(0);
 }
 
+/*
+ * Sets the time to the given UINT32 number
+ */
 void clockSetTime(const UINT32 time)
 {
 	_sr(time,CLOCK_ADDR);
 }
 
+/*
+ * Gets the time of the clock (UINT32)
+ */
 UINT32 clockGetTime()
 {
 	return _lr(CLOCK_ADDR);
