6808086a79a66b915ed7872bacb20159d5c4ec0e
matanpe <matanpe@gmail.com>
2012-05-06 18:59:21 +0000

    Document the code

--- ex1/7segments.c
+++ ex1/7segments.c
@@ -1,6 +1,7 @@
 #include "7segments.h"
 #include "defs.h"
 
+//define all the hex digits
 #define DIGIT_0 0x3f
 #define DIGIT_1 0x06
 #define DIGIT_2 0x5b
@@ -18,22 +19,25 @@
 #define DIGIT_E 0x79
 #define DIGIT_F 0x71
 
-
 #define LEFT_7SEGMENT_BIT 0x80
 #define RIGHT_7SEGMENT_BIT 0x00
 #define SEGMENTS_ADDR 0x104
 
+//array of all the hex digits
 UINT8 digitsMasks[] = {DIGIT_0,DIGIT_1,DIGIT_2,DIGIT_3,DIGIT_4,DIGIT_5,DIGIT_6,DIGIT_7,DIGIT_8,DIGIT_9,DIGIT_A,DIGIT_B,DIGIT_C,DIGIT_D,DIGIT_E,DIGIT_F};
 
+//initilize the 7-segments to "00"
 void segmentsInit()
 {
 	segmentsSetNumber(0);
 }
 
+//sets the 7-segments to the given number
 void segmentsSetNumber(const UINT8 number)
 {
-    
+  //the 4 first digits    
 	const UINT8 rightDigit = digitsMasks[number & 0x0f];
+  //the 4 last digits
 	const UINT8 leftDigit = digitsMasks[(number >> 4) & 0x0f];
 
 	//set the left digit display
