63e0f8495aec828655ac3555ac198b2dbb83f7c0
matanpe <matanpe@gmail.com>
2012-05-06 19:06:09 +0000

    Document to code

--- ex1/clock.h
+++ ex1/clock.h
@@ -3,8 +3,19 @@
 
 #include "defs.h"
 
+/*
+ * Initilize the clock to the time "0"
+ */
 void clockInit();
+
+/*
+ * Sets the time to the given UINT32 number
+ */
 void clockSetTime(const UINT32 time);
+
+/*
+ * Gets the time of the clock (UINT32)
+ */
 UINT32 clockGetTime();
 
 #endif
