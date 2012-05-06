e4fafe124b4a4d38688007ffafdeafcf8dcc821d
matanpe <matanpe@gmail.com>
2012-05-06 19:26:03 +0000

    Document the code

--- ex1/cyclicBuffer.h
+++ ex1/cyclicBuffer.h
@@ -12,8 +12,21 @@ typedef struct
 	UINT8	data[BUFFER_SIZE];
 }CyclicBuffer;
 
+/*
+ * Puts the given UINT8 data into the given cyclic buffer.
+ * the method assumes that the buffer in not full.
+ */
 void cyclicBufferPut(CyclicBuffer * const pBuffer,const UINT8 data);
+
+/*
+ * Gets and pops the data in a FIFO, from the given cyclic buffer.
+ * the method assumes that the buffer in not empty.
+ */
 UINT8 cyclicBufferGet(CyclicBuffer * const pBuffer);
+
+/*
+ * Initilize the given cyclic buffer an empty buffer.
+ */
 void cyclicBufferInit(CyclicBuffer * const pBuffer);
 
 #endif
