229d83a01dab1fe73f6fc3e89af73900ce376b6f
avishai1984@gmail.com <avishai1984@gmail.com@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-05-20 10:54:37 +0000

    timer coding is done

--- trunk/ex2/timer/timer.c
+++ trunk/ex2/timer/timer.c
@@ -1,4 +1,5 @@
 #include "timer.h"
+#include "../common_defs.h"
 
 #define TIMER0_COUNT_ADDR	0x21
 #define TIMER0_CONTROL_ADDR	0x22
@@ -8,53 +9,105 @@
 #define TIMER1_CONTROL_ADDR	0x101
 #define TIMER1_LIMIT_ADDR	0x102
 
-//check if this register need to be set manually or not.
-#define TBR_ADDR			0x75
+#define CPU_FREQ 50*1000000
+#define CYCLES_IN_MS (CPU_FREQ/1000)
 
-void (*gpTimer0CB)();
-void (*gpTimer1CB)();
+#define NUM_OF_TIMERS 2
 
-void timer0Isr()
+uint32_t gCountRegAddr[] 	= 	{TIMER0_COUNT_ADDR,		TIMER1_COUNT_ADDR};
+uint32_t gControlRegAddr[] 	= 	{TIMER0_CONTROL_ADDR,	TIMER1_CONTROL_ADDR};
+uint32_t gLimitRegAddr[] 	= 	{TIMER0_LIMIT_ADDR,		TIMER1_LIMIT_ADDR};
+
+typedef struct
 {
+	union
+	{
+		uint32_t data;
+		struct
+		{
+			uint32_t IE 		: 1;
+			uint32_t NH 		: 1;
+			uint32_t W			: 1;
+			uint32_t IP 		: 1;
+			uint32_t reserve 	: 28;
+		}bits;
+	}control;
 	
+	void (*timerCB)();
+	
+	bool oneShot;
+}Timer;
+
+Timer gTimers[NUM_OF_TIMERS];
+
+void timerInit()
+{
+	int i;
+	for(i = 0 ; i <= NUM_OF_TIMERS ; ++i)
+	{
+		_sr(0,gCountRegAddr[i]);
+		_sr(0,gControlRegAddr[i]);
+		_sr(0,gLimitRegAddr[i]);
+		gTimers[i].data = 0;
+	}
 }
 
-void timer1Isr()
+
+void handleTimerExpiration(uint32_t timerIndex)
 {
+	if (gTimers[timerIndex].oneShot)
+	{
+		gTimers[timerIndex].control.bits.IE = 0;
+	}
+	
+	_sr(gTimers[timerIndex].control.data,gControlRegAddr[timerIndex]);
+	
+	gTimers[timerIndex].timerCB();
+
+}
+
+_Interrupt1 void timer0Isr()
+{
+	handleTimerExpiration(0);
+}
+
+_Interrupt1 void timer1Isr()
+{
+	handleTimerExpiration(1);
+}
+
+result_t registerTimer(	const uint32_t timerIndex,
+						const uint32_t interval, 
+						const bool oneShot, 
+						const void(*timerCB)(void),
+{
+	if (!timerCB)
+	{
+		return NULL_POINTER;
+	}
+	
+	gTimers[timerIndex].timerCB = timerCB;
+	gTimers[timerIndex].oneShot = oneShot;
+	gTimers[timerIndex].control.bits.IE = 1;
+	
+	_sr(0,gCountRegAddr[timerIndex]);
+	_sr(CYCLES_IN_MS * interval,gLimitRegAddr[timerIndex]);
+	//1. we want the timer to generate interrupts (IE=1)
+	//2. we want to timer to be advanced even every clock cycle (NH=0)
+	//3. we don't want the Watchdog to be set (W=0)
+	_sr(gTimers[timerIndex].control.data,gControlRegAddr[timerIndex]);
+	
+	
 	
 }
 
 result_t timer0_register(uint32_t interval, bool one_shot, void(*timer_cb)(void))
 {
-	if (!timer_cb)
-	{
-		return NULL_POINTER;
-	}
-	
-	gpTimer0CB = timer_cb;
-	_sr(0,TIMER0_COUNT_ADDR);
-	_sr(interval,TIMER0_LIMIT_ADDR);
-	//1. we want the timer to generate interrupts
-	//2. we want to timer to be advanced even every clock cycle
-	//3. we don't want the Watchdog to be set
-	_sr(1,TIMER0_CONTROL_ADDR);
+	return registerTimer(0,interval,one_shot,timet_cb);
 }
 
 
 result_t timer1_register(uint32_t interval, bool one_shot, void(*timer_cb)(void))
 {
-	if (!timer_cb)
-	{
-		return NULL_POINTER;
-	}
-	
-	gpTimer1CB = timer_cb;
-	_sr(0,TIMER1_COUNT_ADDR);
-	_sr(interval,TIMER1_LIMIT_ADDR);
-	//1. we want the timer to generate interrupts
-	//2. we want to timer to be advanced even every clock cycle
-	//3. we don't want the Watchdog to be set
-	_sr(1,TIMER1_CONTROL_ADDR);
-	
-	
+	return registerTimer(1,interval,one_shot,timet_cb);
 }
