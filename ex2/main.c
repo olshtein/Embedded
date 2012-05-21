a967f43eecf9e9d1d9160f63eb89eb96d3da8300
matanpe <matanpe@gmail.com>
2012-05-21 17:50:37 +0000

    update the test main.c (didn't checked it)

--- ex2/main.c
+++ ex2/main.c
@@ -6,7 +6,6 @@ int gCounter = 0;
 uint32_t gA = 0;
 uint32_t gB = 0;
 uint32_t gC = 0;
-uint32_t gflushed1 = 1,gflushed2 = 1;
 void incCounter()
 {
     gCounter++; 
@@ -15,21 +14,6 @@ void incCounter()
 button gButton;
 void flushComplete()
 {
-	if(gflushed1)
-	{
-		char line[] = {'e','r','t','Q','!',' ','3','e','$','%','t',')'};
-		lcd_set_row(5, false, line, 12);
-		gflushed1=0;
-	}
-	else
-	{
-		if(gflushed2)
-		{
-			char line[] = {'d','o',' ','i','t','!'};
-			lcd_set_row(17, false, line, 6);
-			gflushed2=0;
-		}
-	}
 }
 
 void buttonPressed(button b)
@@ -44,8 +28,28 @@ void main()
     ip_enable();
     _enable();
     lcd_init(flushComplete);
+    
+    result_t result = NOT_READY;
     char line[] = {'S','o','m','e','_','t','h','i','n','g'};
-    lcd_set_row(4, true, line, 10);
+    while (result == NOT_READY)
+    {
+        result  = lcd_set_row(4, true, line, 10);
+    }
+    
+    result = NOT_READY;
+    char line1[] = {'e','r','t','Q','!',' ','3','e','$','%','t',')'};
+    while (result == NOT_READY)
+    {
+        result  =   lcd_set_row(5, false, line1, 12);
+    }
+    
+    result = NOT_READY;
+    char line2[] = {'d','o',' ','i','t','!'};
+    while (result == NOT_READY)
+    {
+        result  = lcd_set_row(17, false, line2, 6);
+    }
+          
     while(true)
     {
         //counter
