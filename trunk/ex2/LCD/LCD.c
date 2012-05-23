0069011d355792914afcf8c93a1496f5bef8ad0c
matanpe <matanpe@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-05-23 12:07:32 +0000

    update LCD

--- trunk/ex2/LCD/LCD.c
+++ trunk/ex2/LCD/LCD.c
@@ -14,21 +14,17 @@ void (*gpFlushCompleteCB)(void);
 
 //pack the struct with no internal spaces
 #pragma pack(1)
-typedef struct
+typedef union
 {
-	union
+	uint8_t data;
+	struct
 	{
-		uint8_t data;
-		struct
-		{
 			uint8_t charEncode	: 7;
 			uint8_t selected	: 1;
-		}byte;
-	}info;
-	
+	}byte;
 }LcdCharacter;
 
-uint8_t gLcdData[LCD_LINE_LENGTH*LCD_NUM_LINES];
+LcdCharacter gLcdData[LCD_LINE_LENGTH*LCD_NUM_LINES];
 
 #pragma pack()
 
@@ -45,21 +41,21 @@ result_t lcd_init(void (*flush_complete_cb)(void))
 	if(!flush_complete_cb)
 	{
         	return NULL_POINTER;
-    	}
+    }
+
 	//initilize the LCD screen data
 	for(i=0 ; i<LCD_LINE_LENGTH*LCD_NUM_LINES ; ++i)
 	{
-		gLcdData[i] = INIT_CHAR;
+		gLcdData[i].byte.charEncode = INIT_CHAR;
 	}
 	
-    	gpFlushCompleteCB = flush_complete_cb;
-
-    	return OPERATION_SUCCESS;
+    gpFlushCompleteCB = flush_complete_cb;
+    return OPERATION_SUCCESS;
 }
 
 result_t lcd_set_row(uint8_t row_number, bool selected, char const line[], uint8_t length)
 {
-	uint32_t i,bitSelected=0;
+	uint8_t i,bitSelected=0;
 	if(!line)
 	{
 		return NULL_POINTER;
@@ -71,7 +67,7 @@ result_t lcd_set_row(uint8_t row_number, bool selected, char const line[], uint8
 		return INVALID_ARGUMENTS;	
 	}
 
-	if(_lr(LCD_DCMD_ADDR))
+	if(_lr(LCD_DCMD_ADDR)&BIT0)
 	{	
 		return NOT_READY;
 	}
@@ -83,7 +79,7 @@ result_t lcd_set_row(uint8_t row_number, bool selected, char const line[], uint8
 
 	if(selected)
 	{
-		bitSelected = BIT0<<SELECTED_BIT;	
+		bitSelected = 1;	
 	}
 	//note:row_number starts from 0
 	uint32_t rowBytesOffset = LCD_LINE_LENGTH * row_number;
@@ -91,7 +87,8 @@ result_t lcd_set_row(uint8_t row_number, bool selected, char const line[], uint8
 	//write the data to the gLcdData array
 	for(i=0 ; i<length ; ++i)
 	{	
-		gLcdData[rowBytesOffset+i] = line[i] | bitSelected;
+		gLcdData[rowBytesOffset+i].byte.charEncode = line[i];
+		gLcdData[rowBytesOffset+i].byte.selected = bitSelected;
 	}
 
 	//write the LCD data address to the DBUF register
