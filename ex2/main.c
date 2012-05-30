cf2924aec0a2cb68fce06068e946aed64f2adf86
avishai1984 <avishai1984@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-05-30 12:28:35 +0000

    code is ready :-)

--- ex2/main.c
+++ ex2/main.c
@@ -52,7 +52,29 @@ void flashReadDone(uint8_t const *buffer, uint32_t size)
 }
 
 #define BUF_SIZE 70
+void zeroAllFlash()
+{
+	uint8_t buf[MAX_REQUEST_BUFFER_SIZE];
+	result_t res;
+	int i;
 
+	for(i = 0 ; i < MAX_REQUEST_BUFFER_SIZE ; ++i)
+	{
+		buf[i] = 0x0;
+	}
+
+	res = flash_bulk_erase_start();
+	while(!flash_is_ready());
+
+	res = flash_write(MAX_REQUEST_BUFFER_SIZE*128-5,MAX_REQUEST_BUFFER_SIZE,buf);
+
+	for(i = 0 ; i < 127 ; ++i)
+	{
+		res = flash_write(MAX_REQUEST_BUFFER_SIZE*i,MAX_REQUEST_BUFFER_SIZE,buf);
+		i = (i+1)-1;
+	}
+
+}
 result_t flashTest()
 {
 	uint8_t buf[BUF_SIZE];// = {'1','2','1','2','1','2','1','2','1','2'};
@@ -61,6 +83,8 @@ result_t flashTest()
 	uint32_t i = 0;
 	result_t res;
 
+
+
 	for (i = 0 ; i < BUF_SIZE-1 ; ++i)
 	{
 		buf[i] = 't';
@@ -68,6 +92,9 @@ result_t flashTest()
 	buf[BUF_SIZE-1] = 'z';
 
 	flash_init(flashReadDone,flashOpComplete);
+
+	zeroAllFlash();
+
     gFlashOpDone = false;
     flash_bulk_erase_start();
     while(!gFlashOpDone);
@@ -131,20 +158,68 @@ void timerTest()
 		}
 	}
 }
-#define NET_BUF_SIZE 32
+
+
+void txDone(const uint8_t *buffer, uint32_t size)
+{
+
+}
+
+int lcdNum = 0;
+/*
+ * call back when a packet was received.
+ */
+void rxDone(uint8_t buffer[], uint32_t size, uint32_t length)
+{
+	lcd_set_row(lcdNum++,false,(char*)buffer,length);
+}
+
+/*
+ * call back when a packet was dropped during receiving.
+ */
+void rxPacketDrop(packet_dropped_reason_t)
+{
+
+}
+
+/*
+ * call back when a packet was dropped during transmission.
+ */
+void txPacketDrop(transmit_error_reason_t,
+                                           uint8_t *buffer,
+                                           uint32_t size,
+                                           uint32_t length )
+{
+
+}
+
+#define NET_BUF_SIZE 5
 network_init_params_t networkInit;
 desc_t tranBuf[NET_BUF_SIZE];
 desc_t recBuf[NET_BUF_SIZE];
 uint8_t recData[NET_BUF_SIZE*161];
 void networkTest()
 {
-    recBuf[0].pBuffer = (uint32_t)recData;
-    recBuf[0].buff_size = 161;
+	network_call_back_t cbs;
+
+	cbs.dropped_cb = rxPacketDrop;
+	cbs.recieved_cb = rxDone;
+	cbs.transmit_error_cb = txPacketDrop;
+	cbs.transmitted_cb = txDone;
+
+	int i;
+    for(i = 0 ; i<NET_BUF_SIZE ; ++i)
+    {
+    	recBuf[i].pBuffer = (uint32_t)(recData+i*161);
+    	recBuf[i].buff_size = 161;
+
+    }
 
     networkInit.transmit_buffer = tranBuf;
     networkInit.size_t_buffer = NET_BUF_SIZE;
     networkInit.recieve_buffer = recBuf;
     networkInit.size_r_buffer = NET_BUF_SIZE;
+    networkInit.list_call_backs = cbs;
     uint8_t buf[NET_BUF_SIZE];
 
 
@@ -155,21 +230,88 @@ void networkTest()
     network_init(&networkInit);
     network_set_operating_mode(NETWORK_OPERATING_MODE_NORMAL);
 
-    network_send_packet_start((uint8_t*)buf,NET_BUF_SIZE,3);
+    result_t res;
+    for(i = 0 ; i < 10 ; ++i)
+    {
+    	buf[0] = i+33;
+    	res = network_send_packet_start((uint8_t*)buf,NET_BUF_SIZE,3);
+    	buf[0] = i+33;
+    }
 
     return;
+}
+
+int rowNum = 0;
+int charNum = 0;
+bool sel = false;
+#define rowLen 12
+char row[rowLen] = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+void lcdComplete(){}
+char buttonToChar(button but)
+{
+    switch(but)
+    {
+    case BUTTON_OK: return '&';
+    case BUTTON_1: return '1';
+    case BUTTON_2: return '2';
+    case BUTTON_3: return '3';
+    case BUTTON_4: return '4';
+    case BUTTON_5: return '5';
+    case BUTTON_6: return '6';
+    case BUTTON_7: return '7';
+    case BUTTON_8: return '8';
+    case BUTTON_9: return '9';
+    case BUTTON_STAR: return '*';
+    case BUTTON_0: return '0';
+    case BUTTON_NUMBER_SIGN: return '#';
+    }
+    return '%';
+}
+void buttonPressed1(button but)
+{
+    if(charNum>=12)
+    {
+        int i=0;
+        charNum = 0;
+        rowNum++;
+        if(rowNum>=18)
+        {
+            rowNum=0;
+        }
+        for(i=0;i<rowLen;i++)
+        {
+            row[i] = ' ';
+        }
+    }
+
+
+    if(but == BUTTON_STAR) sel = sel ? false : true;
+
+    row[charNum++] = buttonToChar(but);
+    lcd_set_row(rowNum,sel,row,rowLen);
+    //if(but == BUTTON_OK)
+}
+
+testType()
+{
+
+    ip_init(buttonPressed1);
+    ip_enable();
+    lcd_init(lcdComplete);
+    //lcd_set_row(rowNum,sel,row,1);
 }
 
 void main()
 {
     _enable();
-    networkTest();
+    lcd_init(flushComplete);
+    //networkTest();
     //verified - working!
 	//timerTest();
     
-    //lcd_init(flushComplete);
+
     //lcdTest();
-    //flashTest();
+    flashTest();
     while(true)
     {
         //counter
--- ex2/network/network.c
+++ ex2/network/network.c
@@ -1,7 +1,7 @@
 #include "network.h"
 
 #define NETWORK_BASE_ADDR 0x200000
-#define BUFFER_EMPTY(b) gpNetwork->N##b##XFH==gpNetwork->N##b##XFT
+
 //pack the struct with no internal spaces
 #pragma pack(1)
 
@@ -18,45 +18,6 @@ typedef struct
 	uint32_t NRXFT;
 
 	struct
-		{
-			uint8_t NBSY				:1;
-			uint8_t enableTxInterrupt               :1;
-			uint8_t enableRxInterrupt               :1;
-			uint8_t enablePacketDroppedInterrupt    :1;
-			uint8_t enableTransmitErrorInterrupt    :1;
-			uint32_t reserved                       :24;
-                        uint8_t NOM                             :3;
-                }NCTRL;
-
-		struct
-		{
-				uint8_t NTIP            :1;
-				uint8_t reserved1       :1;
-				uint8_t NRIP            :1;
-				uint8_t NROR            :1;
-				uint8_t reserved2       :4;
-					union
-					{
-						uint8_t data		:8;
-						struct
-						{
-							uint8_t RxComplete              :1;
-							uint8_t TxComplete              :1;
-							uint8_t RxBufferTooSmall        :1;
-							uint8_t CircularBufferFull      :1;
-							uint8_t TxBadDescriptor         :1;
-							uint8_t TxNetworkError          :1;
-							uint8_t reserved                :2;
-						}bits;
-					}NIRE;
-					uint16_t reserved       :16;
-				}NSTAT;
-
-
-}volatile NetworkRegs;
-
-/*
-typedef struct
 	{
 		uint8_t NBSY							:1;
 		uint8_t enableTxInterrupt               :1;
@@ -64,213 +25,66 @@ typedef struct
 		uint8_t enablePacketDroppedInterrupt    :1;
 		uint8_t enableTransmitErrorInterrupt    :1;
 		uint32_t reserved                       :24;
+		uint8_t NOM                             :3;
+	}NCTRL;
+
+	struct
+	{
+		uint8_t NTIP            :1;
+		uint8_t reserved1       :1;
+		uint8_t NRIP            :1;
+		uint8_t NROR            :1;
+		uint8_t reserved2       :4;
 		union
 		{
-			uint8_t data                        :3;
+			uint8_t data		:8;
 			struct
 			{
-				uint8_t normal                  :1;
-				uint8_t SmscConnectivity    	:1;
-				uint8_t internalLoopback        :1;
+				uint8_t RxComplete              :1;
+				uint8_t TxComplete              :1;
+				uint8_t RxBufferTooSmall        :1;
+				uint8_t CircularBufferFull      :1;
+				uint8_t TxBadDescriptor         :1;
+				uint8_t TxNetworkError          :1;
+				uint8_t reserved                :2;
 			}bits;
-		}NOM;
-	}NCTRLReg;
-
-	typedef struct
-		{
-		uint32_t NTIP            :1;
-		uint32_t reserved1       :1;
-		uint32_t NRIP            :1;
-		uint32_t NROR            :1;
-		uint32_t reserved2       :4;
-			union
-			{
-			uint32_t data		:8;
-				struct
-				{
-				uint32_t RxComplete              :1;
-				uint32_t TxComplete              :1;
-				uint32_t RxBufferTooSmall        :1;
-				uint32_t CircularBufferFull      :1;
-				uint32_t TxBadDescriptor         :1;
-				uint32_t TxNetworkError          :1;
-				uint32_t reserved                :2;
-				}bits;
-			}NIRE;
-			uint32_t reserved       :16;
-		}NSTATReg;
-
-		typedef struct
-				{
-				uint32_t NTIP            :1;
-				uint32_t reserved1       :1;
-				uint32_t NRIP            :1;
-				uint32_t NROR            :1;
-				uint32_t reserved2       :4;
-					union
-					{
-					uint32_t data		:8;
-						struct
-						{
-						uint32_t RxComplete              :1;
-						uint32_t TxComplete              :1;
-						uint32_t RxBufferTooSmall        :1;
-						uint32_t CircularBufferFulll     :1;
-						uint32_t TxBadDescriptor         :1;
-						uint32_t TxNetworkError          :1;
-						uint32_t reserved                :2;
-						}bits;
-					}NIRE;
-					uint32_t reserved       :16;
-				}TestStruct;
-
-
-typedef struct
-{
-	desc_t* NTXBP;
-	uint32_t NTXBL;
-	uint32_t NTXFH;
-	uint32_t NTXFT;
-
-	desc_t* NRXBP;
-	uint32_t NRXBL;
-	uint32_t NRXFH;
-	uint32_t NRXFT;
-
-	NCTRLReg NCTRL;
-
-	NSTATReg NSTAT;
-
-
+		}NIRE;
+		uint16_t reserved       :16;
+	}NSTAT;
 }volatile NetworkRegs;
 
 
-
-typedef struct
-{
-	desc_t* NTXBP;
-	uint32_t NTXBL;
-	uint32_t NTXFH;
-	uint32_t NTXFT;
-
-	desc_t* NRXBP;
-	uint32_t NRXBL;
-	uint32_t NRXFH;
-	uint32_t NRXFT;
-
-}volatile NetworkRegs1;
-
-NetworkRegsOld gTest;
-*/
-
-typedef struct
-{
-	desc_t* NTXBP;
-	uint32_t NTXBL;
-	uint32_t NTXFH;
-	uint32_t NTXFT;
-
-	desc_t* NRXBP;
-	uint32_t NRXBL;
-	uint32_t NRXFH;
-	uint32_t NRXFT;
-
-	union
-	{
-		uint32_t data;
-            	struct
-		{
-			uint8_t NBSY							:1;
-			uint8_t enableTxInterrupt               :1;
-			uint8_t enableRxInterrupt               :1;
-			uint8_t enablePacketDroppedInterrupt    :1;
-			uint8_t enableTransmitErrorInterrupt    :1;
-			uint32_t reserved                       :24;
-                    union
-                    {
-				uint8_t data						:3;
-                        /*struct
-				{
-					uint8_t normal                  :1;
-					uint8_t SmscConnectivity    	:1;
-					uint8_t internalLoopback        :1;
-                                        }bits;*/
-                    }NOM
-                }bits;
-	}NCTRL;
-
-
-
-	union
-	{
-		uint32_t data;
-            struct
-		{
-			uint8_t NTIP            :1;
-			uint8_t reserved1       :1;
-			uint8_t NRIP            :1;
-			uint8_t NROR            :1;
-			uint8_t reserved2       :4;
-			union
-			{
-				uint8_t data;
-				struct
-				{
-					uint8_t RxComplete              :1;
-					uint8_t TxComplete              :1;
-					uint8_t RxBufferTooSmall        :1;
-					uint8_t CircularBufferFull      :1;
-					uint8_t TxBadDescriptor         :1;
-					uint8_t TxNetworkError          :1;
-					uint8_t reserved                :2;
-				}bits;
-			}NIRE;
-			uint16_t reserved       :16;
-                        }bits;
-            
-	}NSTAT;
-
-
-}volatile NetworkRegsNew;
 #pragma pack()
 
 
-void test()
-{
-	uint32_t a;
-	a = sizeof(NetworkRegs);
-	a = a * 10;
-//	//NetworkRegs tt;
-//	a = sizeof(NetworkRegs1);
-//
-//	b = sizeof(NCTRLReg);
-//	c = sizeof(NSTATReg);
-//	d = sizeof(NetworkRegsOld);
-//	d = a+b+c;
-}
 
 //cost the NETWORK_BASE_ADDR to the NetworkRegs struct
 NetworkRegs* gpNetwork = (NetworkRegs*)(NETWORK_BASE_ADDR);
 
-network_call_back_t gNetworkCallBacks;
+network_call_back_t gNetworkCallBacks = {0};
 
-//uint32_t gNetworkData[NETWORK_MAXIMUM_TRANSMISSION_UNIT];
-
+//check that all cb pointers valid
+bool isValidCallbaks(network_call_back_t cb)
+{
+	return (cb.dropped_cb && cb.recieved_cb && cb.transmit_error_cb && cb.transmitted_cb);
+}
 
 result_t network_init(const network_init_params_t *init_params)
 {
-	test();
-	if(!init_params || !init_params->transmit_buffer || !init_params->recieve_buffer)
+	//check that all pointers not null
+	if(	!init_params || !init_params->transmit_buffer || !init_params->recieve_buffer ||
+		!isValidCallbaks(init_params->list_call_backs))
 	{
 		return NULL_POINTER;
 	}
 
+	//check minimal buffer size
 	if(init_params->size_t_buffer<2 || init_params->size_r_buffer<2)
 	{
 		return INVALID_ARGUMENTS;
 	}
 
-	//                      gNetworkParams = *init_params;
+	//set parameters
 	gNetworkCallBacks = init_params->list_call_backs;
 	gpNetwork->NTXBP = init_params->transmit_buffer;
 	gpNetwork->NTXBL = init_params->size_t_buffer;
@@ -282,6 +96,7 @@ result_t network_init(const network_init_params_t *init_params)
 	gpNetwork->NRXFH = 0;
 	gpNetwork->NRXFT = 0;
 
+	//enable interrupts
 	gpNetwork->NCTRL.enableTxInterrupt = 1;
 	gpNetwork->NCTRL.enableRxInterrupt = 1;
 	gpNetwork->NCTRL.enablePacketDroppedInterrupt = 1;
@@ -293,10 +108,12 @@ result_t network_init(const network_init_params_t *init_params)
 
 result_t network_set_operating_mode(network_operating_mode_t new_mode)
 {
-	//if()
-	//return INVALID_ARGUMENTS;
+	if(new_mode > NETWORK_OPERATING_MODE_INTERNAL_LOOPBACK)
+	{
+		return INVALID_ARGUMENTS;
+	}
 
-	gpNetwork->NCTRL.NOM = (new_mode);
+	gpNetwork->NCTRL.NOM = new_mode;
 
 	return OPERATION_SUCCESS;
 }
@@ -319,9 +136,6 @@ result_t network_send_packet_start(const uint8_t buffer[], uint32_t size, uint32
 	}
 
 
-	//desc_t* headOffset = (desc_t*)((uint32_t)gpNetwork->NTXFH - (uint32_t)gpNetwork->NTXBP);
-	//desc_t* tailOffset  = (desc_t*)((uint32_t)gpNetwork->NTXFT - (uint32_t)gpNetwork->NTXBP);
-
 	if( (gpNetwork->NTXFH +1)%gpNetwork->NTXBL == gpNetwork->NTXFT )
 	{
 		return NETWORK_TRANSMIT_BUFFER_FULL;
@@ -341,73 +155,78 @@ result_t network_send_packet_start(const uint8_t buffer[], uint32_t size, uint32
 	return OPERATION_SUCCESS;
 }
 
-desc_t* getLastDesc(uint32_t tail, desc_t* bufferBase, uint32_t bufferSize)
-{
-	uint32_t tailOffset = (tail+bufferSize-1)%bufferSize;
-	return gpNetwork->NRXBP+tailOffset;
-	//desc_t* tailOffset = (desc_t*)(tail-bufferBase);
-	//return  (desc_t*)((uint32_t)(tailOffset + bufferSize -1)%bufferSize);
-}
 
 _Interrupt1 void networkISR()
 {
-	if(gpNetwork->NSTAT.NIRE.bits.RxComplete)
+	//Important note. the order of those events important since in Loopback mode if we handle
+	//Rx before Tx, Rx will be asserted again, need to Ack Tx before Ack Rx
+	if(gpNetwork->NSTAT.NIRE.bits.TxComplete)
 	{
-		//Acknowledging the interrupt
-		gpNetwork->NSTAT.NIRE.bits.RxComplete = 1;
-
-		//desc_t* tailOffset  = (desc_t*)((uint32_t)gpNetwork->NRXFT - (uint32_t)gpNetwork->NRXBP);
-		//desc_t* lastDesc = (desc_t*)( ((uint32_t)(tailOffset + gpNetwork->NRXBL - 1))%gpNetwork->NRXBL);
-		//desc_t* lastDesc = getLastDesc(gpNetwork->NRXFT,gpNetwork->NRXBP,gpNetwork->NRXBL);
-		desc_t* pPacket = gpNetwork->NRXBP+gpNetwork->NRXFT;
-
-                //call cb
-                gNetworkCallBacks.recieved_cb((uint8_t*)pPacket->pBuffer,pPacket->buff_size,pPacket->reserved & 0xFF);
-
-		//increase the head pointer
-		gpNetwork->NRXFT = (gpNetwork->NRXFT + 1)%gpNetwork->NRXBL;
-
-	}
-	else if(gpNetwork->NSTAT.NIRE.bits.TxComplete)
-	{
-		//Acknowledging the interrupt
-		gpNetwork->NSTAT.NIRE.bits.TxComplete = 1;
 
 		desc_t* pPacket = gpNetwork->NTXBP+gpNetwork->NTXFT;
 
 		//advance tail cyclically by one
 		gpNetwork->NTXFT = (gpNetwork->NTXFT+1)%gpNetwork->NTXBL;
 
+		//Acknowledging the interrupt
+		gpNetwork->NSTAT.NIRE.bits.TxComplete = 1;
+
 		//call cb
 		gNetworkCallBacks.transmitted_cb((uint8_t*)pPacket->pBuffer,pPacket->buff_size);
 
 	}
+	else if(gpNetwork->NSTAT.NIRE.bits.RxComplete)
+	{
+		desc_t* pPacket = gpNetwork->NRXBP+gpNetwork->NRXFT;
+
+		//increase the head pointer
+		gpNetwork->NRXFT = (gpNetwork->NRXFT + 1)%gpNetwork->NRXBL;
+
+		//Acknowledging the interrupt
+		gpNetwork->NSTAT.NIRE.bits.RxComplete = 1;
+
+        //call cb
+        gNetworkCallBacks.recieved_cb((uint8_t*)pPacket->pBuffer,pPacket->buff_size,pPacket->reserved & 0xFF);
+
+
+	}
 	else if(gpNetwork->NSTAT.NIRE.bits.RxBufferTooSmall)
 	{
+		//Ack the interrupt
 		gpNetwork->NSTAT.NIRE.bits.RxBufferTooSmall = 1;
 		gNetworkCallBacks.dropped_cb(RX_BUFFER_TOO_SMALL);
 
 	}
 	else if(gpNetwork->NSTAT.NIRE.bits.CircularBufferFull)
 	{
+		//Ack the interrupt
 		gpNetwork->NSTAT.NIRE.bits.CircularBufferFull = 1;
 		gNetworkCallBacks.dropped_cb(CIRCULAR_BUFFER_FULL);
 	}
 	else if(gpNetwork->NSTAT.NIRE.bits.TxBadDescriptor)
 	{
-		gpNetwork->NSTAT.NIRE.bits.TxBadDescriptor = 1;
+
 		desc_t* pPacket = gpNetwork->NTXBP+gpNetwork->NTXFT;
 
 		//advance tail cyclically by one
 		gpNetwork->NTXFT = (gpNetwork->NTXFT+1)%gpNetwork->NTXBL;
+
+		//Ack the interrupt
+		gpNetwork->NSTAT.NIRE.bits.TxBadDescriptor = 1;
 
 		//call cb
 		gNetworkCallBacks.transmit_error_cb(BAD_DESCRIPTOR,(uint8_t*)pPacket->pBuffer,pPacket->buff_size,pPacket->reserved & 0xFFFF);
 	}
 	else if(gpNetwork->NSTAT.NIRE.bits.TxNetworkError)
 	{
-		gpNetwork->NSTAT.NIRE.bits.TxNetworkError = 1;
+
 		desc_t* pPacket = gpNetwork->NTXBP+gpNetwork->NTXFT;
+
+		//advance tail cyclically by one
+		gpNetwork->NTXFT = (gpNetwork->NTXFT+1)%gpNetwork->NTXBL;
+
+		//Ack the interrupt
+		gpNetwork->NSTAT.NIRE.bits.TxNetworkError = 1;
 
 		//call cb
 		gNetworkCallBacks.transmit_error_cb(NETWORK_ERROR,(uint8_t*)pPacket->pBuffer,pPacket->buff_size,pPacket->reserved & 0xFFFF);
