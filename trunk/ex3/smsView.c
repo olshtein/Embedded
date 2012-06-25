45dec75b64f285974c73eb1a65a746d3cf76f642
avishai1984 <avishai1984@9e09d17a-9e13-eb8b-c868-f9760a9c16fb>
2012-06-25 21:49:29 +0000

    update README and fix includes of the given sms protocol to our protocol

--- trunk/ex3/smsView.c
+++ trunk/ex3/smsView.c
@@ -1,7 +1,7 @@
 #include "smsView.h"
 #include "smsModel.h"
 #include "TX/tx_api.h"
-#include "embsys_sms_protocol.h"
+#include "embsys_sms_protocol_mine.h"
 #include <string.h>
 #include "LCD/LCD.h"
 #include "common_defs.h"
